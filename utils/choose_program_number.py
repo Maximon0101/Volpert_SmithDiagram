0ca790fb286d507b4dfe542d516fff1494a3405b
Maximon0101 <121817587+Maximon0101@users.noreply.github.com>
2025-10-14 17:57:10 +0300

    feat: find_Zin.py now added

--- utils/choose_program_number.py
+++ utils/choose_program_number.py
@@ -1,14 +1,17 @@
-from matching_by_train import matching_by_train
+from programs.find_Zin import find_Zin
+from programs.matching_by_train import matching_by_train
 from utils.input_program_number import input_program_number
 
 
 def choose_program_number():
-    programs_count = 2
+    programs_count = 3
 
     number_of_program = input_program_number(programs_count)
 
     match number_of_program:
         case 1:
-            matching_by_train()
+            find_Zin()
         case 2:
+            matching_by_train()
+        case 3:
             print("Aboba")
--- utils/input_program_number.py
+++ utils/input_program_number.py
@@ -1,13 +1,14 @@
 def input_program_number(programs_count: int) -> int:
     number_of_program = simple_input_program_number()
 
-    if number_of_program not in range(1, programs_count + 1):
+    while number_of_program not in range(1, programs_count + 1):
         print("\nВы ввели некорректное значение. Попробуйте ещё раз")
         number_of_program = simple_input_program_number()
     return number_of_program
 
 def simple_input_program_number() -> int:
     print("Введите номер исполняемой программы:\n"
-          "[1] - Согласовании линии с комплексной нагрузкой при помощи шлейфа\n"
-          "[2] - Aboba\n")
+          "[1] - Поиск входного сопротивления на расстоянии от нагрузки\n"
+          "[2] - Согласовании линии с комплексной нагрузкой при помощи шлейфа\n"
+          "[3] - Aboba\n")
     return int(input("Поле для ввода: "))
